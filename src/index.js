97b6817164cb3cafc98d06967521f084cac51ab6
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-11-03 15:22:14 +0100

    feat: debounced input changing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "./styles.css";
+import { debounce } from "./utils";
+
+const INPUT_PLACEHOLDER = "Location";
 
 class API {
   constructor() {
@@ -17,18 +20,33 @@ class API {
     )}`;
     const data = await fetch(url);
     const response = await data.json();
+    console.log(response);
     return response;
   }
 }
 
 class View {
   constructor() {}
+  render(data) {}
+  bindLocationInput(handler) {
+    const input = document.getElementById("location");
+    input.addEventListener("input", debounce(handler, 500));
+  }
 }
 
 class Controller {
   constructor(api, view) {
     this.api = api;
     this.view = view;
+    this.location = undefined;
+    this.updateView();
+  }
+  updateView(data) {
+    this.view.render(data);
+    this.view.bindLocationInput(this.handleLocationInput.bind(this));
+  }
+  handleLocationInput(e) {
+    console.log(e.target.value);
   }
 }
 
