50a70490e69b709f8d09c05c68bf44c084b192b5
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2024-11-04 14:54:45 +0100

    feat: landing page.

--- src/index.js
+++ src/index.js
@@ -35,7 +35,6 @@ class API {
     )}`;
     const data = await fetch(url);
     const response = await data.json();
-    // console.log(response);
     return response;
   }
 }
@@ -51,9 +50,14 @@ class View {
     this.renderDescription(data);
     this.renderBackgroundImage(data);
     this.renderClock(data);
+    this.renderContainer();
     if (data) {
       console.log("from render", data);
     }
+  }
+  renderContainer() {
+    const container = document.querySelector(".container");
+    container.style.display = "flex";
   }
   renderLocation(data) {
     const locationContainer = document.querySelector(".main__location");
@@ -89,11 +93,28 @@ class View {
     }
   }
   renderLocationInput() {
-    const locationInput = document.getElementById("location");
-    if (locationInput) {
-      locationInput.remove();
+    const locationForm = document.querySelector("form");
+    if (locationForm) {
+      locationForm.remove();
     }
     const html = `
+    <form>
+    <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          id="search-icon"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+          <path d="M21 21l-6 -6" />
+        </svg>
     <input 
       type="text" 
       name="location" 
@@ -101,8 +122,9 @@ class View {
       placeholder="Enter Location"
       value="${this.location}"
     />
+    </form>
     `;
-    document.querySelector("form").insertAdjacentHTML("beforeend", html);
+    document.querySelector("header").insertAdjacentHTML("beforeend", html);
   }
   renderBackgroundImage(data) {
     const bgImg = document.getElementById("bg-img");
@@ -241,7 +263,8 @@ class Controller {
   constructor(api, view) {
     this.api = api;
     this.view = view;
-    this.updateView(null);
+    // this.updateView(null);
+    this.view.bindLocationInput(this.handleLocationInput.bind(this));
   }
   updateView(data) {
     this.view.render(data);
